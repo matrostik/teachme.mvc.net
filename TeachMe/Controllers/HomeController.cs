b0e5110b9a77c32d1bb9f57bbfa98fb54333a185
matrostik <matrostik@gmail.com>
2014-03-17 23:32:40 +0200

    Contact send email added

--- TeachMe/Controllers/HomeController.cs
+++ TeachMe/Controllers/HomeController.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mail;
 using System.Web;
 using System.Web.Mvc;
+using TeachMe.Models;
 
 namespace TeachMe.Controllers
 {
@@ -25,6 +27,39 @@ namespace TeachMe.Controllers
             ViewBag.Message = "TeachMe contact page.";
 
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Contact([Bind(Include = "From,Subject,Body")] MailModel mail)
+        {
+            if (ModelState.IsValid)
+            {
+                MailMessage email = new MailMessage();
+                email.To.Add("matrostik@gmail.com,kobieliasi@gmail.com,anttross@gmail.com,ahuvabloy@gmail.com");
+                email.From = new MailAddress(mail.From);
+                email.Subject = mail.Subject;
+                string Body = mail.Body;
+                email.Body = Body;
+                email.IsBodyHtml = true;
+                SmtpClient smtp = new SmtpClient();
+                smtp.Host = "smtp.gmail.com";
+                smtp.Port = 587;
+                smtp.UseDefaultCredentials = false;
+                smtp.Credentials = new System.Net.NetworkCredential
+                ("jce.teachme", "jcesoft2014");// Enter seders User name and password  
+                smtp.EnableSsl = true;
+                try
+                {
+                    smtp.Send(email);
+                    ViewBag.Result = "Success";
+                }
+                catch (Exception)
+                {
+                    ViewBag.Result = "Error";
+                } 
+            }
+
+            return View(new MailModel());
         }
     }
 }
---
+++ TeachMe/Models/MailModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TeachMe.Models
+{
+    public class MailModel
+    {
+        [Required(ErrorMessage = "* From required")]
+        [RegularExpression(@"^(([A-Za-z0-9]+_+)|([A-Za-z0-9]+\-+)|([A-Za-z0-9]+\.+)|([A-Za-z0-9]+\++))*[A-Za-z0-9]+@((\w+\-+)|(\w+\.))*\w{1,63}\.[a-zA-Z]{2,6}$",ErrorMessage="* Not a valid email ")]
+        public string From { get; set; }
+        public string To { get; set; }
+        [Required(ErrorMessage = "* Subject required")]
+        public string Subject { get; set; }
+        [Required(ErrorMessage = "* Body required")]
+        public string Body { get; set; }
+    }
+}

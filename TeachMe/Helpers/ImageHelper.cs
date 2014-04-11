cd6adf62f9b77afd1220edc723e460a95e05f5ba
matrostik <matrostik@gmail.com>
2014-04-11 14:53:32 +0300

    custom ActionLink helper with Glyphicons

---
+++ TeachMe/Helpers/ImageHelper.cs
@@ -0,0 +1,31 @@
+﻿using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace TeachMe.Helpers
+{
+    public static class ImageHelper
+    {
+        public static MvcHtmlString Image(this HtmlHelper helper, string id, string url, string alternateText)
+        {
+            return Image(helper, id, url, alternateText, null);
+        }
+
+        public static MvcHtmlString Image(this HtmlHelper helper, string id, string url, string alternateText, object htmlAttributes)
+        {
+            // Create tag builder
+            var builder = new TagBuilder("img");
+
+            // Create valid id
+            builder.GenerateId(id);
+
+            // Add attributes
+            builder.MergeAttribute("src", url);
+            builder.MergeAttribute("alt", alternateText);
+            builder.MergeAttributes(new RouteValueDictionary(htmlAttributes));
+
+            // Render tag
+            return MvcHtmlString.Create(builder.ToString(TagRenderMode.SelfClosing));
+        }
+
+    }
+}

e701a12cd5dd6d35290cb37fbcee0be7bf6877dc
Kobi <kobieliasi@gmail.com>
2014-04-13 06:04:16 +0300

    translate the Mail Model

--- TeachMe/Models/MailModel.cs
+++ TeachMe/Models/MailModel.cs
@@ -4,13 +4,13 @@ namespace TeachMe.Models
 {
     public class MailModel
     {
-        [Required(ErrorMessage = "* From required")]
-        [RegularExpression(@"^(([A-Za-z0-9]+_+)|([A-Za-z0-9]+\-+)|([A-Za-z0-9]+\.+)|([A-Za-z0-9]+\++))*[A-Za-z0-9]+@((\w+\-+)|(\w+\.))*\w{1,63}\.[a-zA-Z]{2,6}$",ErrorMessage="* Not a valid email ")]
+        [Required(ErrorMessage = "* יש להכניס דוא\"ל")]
+        [RegularExpression(@"^(([A-Za-z0-9]+_+)|([A-Za-z0-9]+\-+)|([A-Za-z0-9]+\.+)|([A-Za-z0-9]+\++))*[A-Za-z0-9]+@((\w+\-+)|(\w+\.))*\w{1,63}\.[a-zA-Z]{2,6}$",ErrorMessage="* כתובת דוא\"ל לא חוקית ")]
         public string From { get; set; }
         public string To { get; set; }
-        [Required(ErrorMessage = "* Subject required")]
+        [Required(ErrorMessage = "* יש להכניס נושא")]
         public string Subject { get; set; }
-        [Required(ErrorMessage = "* Body required")]
+        [Required(ErrorMessage = "* יש להכניס תוכן")]
         public string Body { get; set; }
     }
 }

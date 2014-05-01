bc9121b77ecd35d17fb5ad68a50e8513537cd790
anttross <anttross@gmail.com>
2014-05-01 23:05:52 +0300

    model for Subject

---
+++ TeachMe/Models/Subject.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TeachMe.Models
+{
+    public class Subject
+    {
+
+        #region Fields
+
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+        
+        #endregion
+
+    }
+}

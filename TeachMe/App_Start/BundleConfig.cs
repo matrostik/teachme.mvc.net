f819e35d47145fd6eefd5f3c876d1858dbfce91d
matrostik <matrostik@gmail.com>
2014-06-05 23:32:51 +0300

    css clean + main page animation

--- TeachMe/App_Start/BundleConfig.cs
+++ TeachMe/App_Start/BundleConfig.cs
@@ -32,17 +32,19 @@ namespace TeachMe
             bundles.Add(new StyleBundle("~/Content/css1").Include(
                       "~/Content/amelia.css",
                       "~/Content/site.css"));
-            // new css
+
+            // UnifyTheme css
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.min.css", "~/Content/UnifyTheme/app.css", "~/Content/UnifyTheme/plugins.css")
-                      .Include("~/Content/UnifyTheme/style.css", new CssRewriteUrlTransform())
-                      .Include("~/Content/UnifyTheme/default.css",
-                       "~/Content/UnifyTheme/custom.css"));
-            bundles.Add(new StyleBundle("~/Content/cssplugins").Include(
-                     "~/Content/UnifyTheme/plugins/flexslider.css",
-                     "~/Content/UnifyTheme/plugins/parallax-slider.css")
+                      "~/Content/bootstrap.min.css")
+                      .Include("~/Content/UnifyTheme/*.css", new CssRewriteUrlTransform()));
+
+            // UnifyTheme plugins css
+            bundles.Add(new StyleBundle("~/Content/cssplugins")
                      .Include("~/Content/UnifyTheme/plugins/font-awesome.min.css", new CssRewriteUrlTransform())
-                     .Include("~/Content/UnifyTheme/plugins/line-icons.css", new CssRewriteUrlTransform()));
+                     .Include("~/Content/UnifyTheme/plugins/line-icons.css", new CssRewriteUrlTransform())
+                     .Include("~/Content/UnifyTheme/plugins/flexslider.css","~/Content/UnifyTheme/plugins/parallax-slider.css")
+                     .Include("~/Content/UnifyTheme/plugins/box-shadows.css", "~/Content/UnifyTheme/plugins/animate.css"));
+
             bundles.Add(new ScriptBundle("~/bundles/unify/page").Include(
                       "~/Scripts/UnifyTheme/jquery-migrate-1.2.1.min.js",
                        "~/Scripts/UnifyTheme/pages/app.js",
@@ -52,11 +54,8 @@ namespace TeachMe
                       "~/Scripts/UnifyTheme/jquery.flexslider-min.js",
                       "~/Scripts/UnifyTheme/modernizr.js",
                       "~/Scripts/UnifyTheme/jquery.cslider.js"));
-            bundles.Add(new StyleBundle("~/Content/page-reg").Include(
-                   "~/Content/UnifyTheme/pages/page_log_reg_v1.css"));
-            bundles.Add(new StyleBundle("~/Content/page-search").Include(
-                  "~/Content/UnifyTheme/pages/page_search_inner.css"));
-            //
+
+            // Other plugins
             bundles.Add(new ScriptBundle("~/bundles/fileupload").Include(
                      "~/Scripts/jquery.fileupload.js",
                      "~/Scripts/jquery.ui.widget.js"));
